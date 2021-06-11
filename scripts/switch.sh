a35523a248880cd6b8c719948546bf6561c066eb
hak2711 <hak271111@gmail.com>
2021-06-11 20:51:31 +0900

    무중단 배포 테스트 재시도

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -10,5 +10,4 @@ function switch_proxy() {
     echo "> Port 전환"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT}; | sudo tee /etc/nginx/conf.d/service-url.inc"
     echo "> 엔진엑스 reload"
-    sudo service nginx reload
-}
+    nginx -s reload
